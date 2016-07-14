6a0a7236ff5d31fd12d8ab0fb0f3153fd22cee91
chad <chapnickc@gmail.com>
2016-07-14 00:53:46 -0500

    -

---
+++ p3/functions/convert_levels.R
@@ -0,0 +1,9 @@
+convert_levels <- function(column, new_levels){
+
+  new <- as.character(column)
+  new[which(new == "<0.1")] <- '0.055'
+  
+  new <- as.numeric(new)
+  result <- cut(new, new_levels)
+  return(result)
+}
